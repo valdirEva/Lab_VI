c6ff08c80cdb9fda73f7cadb8afedbfbdfde82e9
valdirEva <valdirevaristo.silva@gmail.com>
2020-09-13 19:14:14 -0300

    Segundo commit

--- entrega_introducao/src/main/app/Aplicacao.java
+++ entrega_introducao/src/main/app/Aplicacao.java
@@ -4,7 +4,7 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		CadastroRg rg = new CadastroRg();
-		
+		System.out.print(rg.isValidaRG("12864011-28"));
 
 	}
 
--- entrega_introducao/src/main/model/CadastroRg.java
+++ entrega_introducao/src/main/model/CadastroRg.java
@@ -1,5 +1,24 @@
 package model;
 
 public class CadastroRg {
-	
+	public boolean isValidaRG(String rg){
+
+		if((rg== null) ){
+
+			return false;
+
+		}
+		if(rg.length()!=11){
+
+			return false;
+
+		}
+		if(rg.isEmpty()){
+
+			return false;
+
+		}
+
+		return true;
+	}
 }
